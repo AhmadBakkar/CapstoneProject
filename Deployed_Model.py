3a01b7d56126e822f966d409ca45d441b2872a41
AhmadBakkar <90552904+AhmadBakkar@users.noreply.github.com>
2024-04-29 19:13:43 +0300

    Update Deployed_Model.py

--- Deployed_Model.py
+++ Deployed_Model.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
 import joblib
 from sklearn.compose import ColumnTransformer, make_column_selector
 from sklearn.pipeline import Pipeline
