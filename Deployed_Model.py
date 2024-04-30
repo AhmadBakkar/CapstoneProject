d253a2670465b95391c7452f9cf1f50d07d5d026
AhmadBakkar <90552904+AhmadBakkar@users.noreply.github.com>
2024-04-30 08:26:11 +0300

    Update Deployed_Model.py

--- Deployed_Model.py
+++ Deployed_Model.py
@@ -7,11 +7,11 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from joblib import dump
 from joblib import load
 
-filename = "trained_model_awarded.joblib"
+#filename = ""
 #filename1 = "trained_model_2.sav"
 
-with open(filename, 'rb') as f:
-    model = joblib.load(f)
+#with open(filename, 'rb') as f:
+    model = joblib.load(trained_model_awarded.joblib)
 
 #with open(filename1,'rb') as f1:
 #    model1 = pickle.load(f1)
